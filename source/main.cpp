5d0a7ad404ca7782f9336a5b44385857e847eca9
Nalin Dalal <nalindalal2004@gmail.com>
2026-07-23 15:58:28 +0530

    help guide

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,43 @@
 
 #include "cut-cpp/cut.hpp"
 
+auto print_usage() -> void
+{
+  std::cout << "Usage: cut -f LIST [-d DELIM] [-s] [FILE...]\n"
+            << "       cut -c LIST [-s] [FILE...]\n"
+            << "\n"
+            << "Print selected parts of lines from each FILE to standard output.\n"
+            << "\n"
+            << "Options:\n"
+            << "  -f, --fields=LIST      select only these fields\n"
+            << "  -c, --characters=LIST  select only these characters\n"
+            << "  -d, --delimiter=DELIM  use DELIM instead of TAB as field delimiter\n"
+            << "  -s, --only-delimited   do not print lines not containing delimiters\n"
+            << "      --help             display this help and exit\n"
+            << "\n"
+            << "LIST is comma-separated numbers or ranges. Ranges are N-M (from N to M),\n"
+            << "N- (from N to end), or -M (from start to M).\n"
+            << "\n"
+            << "Examples:\n"
+            << "  cut -f 1,3          print fields 1 and 3\n"
+            << "  cut -f 2-           print from field 2 to end\n"
+            << "  cut -c 1-5          print characters 1 through 5\n"
+            << "  cut -d : -f 3 file  print field 3 using : as delimiter\n";
+}
+
 auto main(int argc, char const* argv[]) -> int
 {
+  for (int i = 1; i < argc; i++)
+  {
+    std::string arg = argv[i];
+
+    if (arg == "--help")
+    {
+      print_usage();
+      return 0;
+    }
+  }
+
   cut_parser parser;
 
   if (!parser.parse(argc, argv))
